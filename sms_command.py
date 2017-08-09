4934c8ac258594d868b9ef5e46b98fff54e6095a
DENNIS KLINE <dennis.r.kline@jpmchase.com>
2017-08-09 06:53:06 -0400

    Split PI class into smaller classes to try and avoid GPIO conflicts between scripts

--- sms_command.py
+++ sms_command.py
@@ -13,6 +13,10 @@ from gdmod import Database
 from gdmod import Email
 from gdmod import NetworkDownException
 from gdmod import Pi
+from gdmod import Camera
+from gdmod import Light
+from gdmod import DoorRemote
+from gdmod import DoorState
 from gdmod import Sms
 from gdmod import GarageDoorCommand
 from gdmod import Dropbox
@@ -40,19 +44,23 @@ def main():
     az = Authorization(config.get('sms.door.command.allowed.phonenumbers'))
     db = Database(config.get('app.database.file'))
     pi = Pi()
+    doorRemote = DoorRemote()
+    doorState = DoorState()
+    camera = Camera()
+    light = Light()
     sms = Sms(db, config.get('sms.account.id'), config.get('sms.account.token'), config.get('sms.account.phone.number'))
     #sms.twilioClient.isMock = True # TODO
     #sms.twilioClient.mockMessages = __mock_messages() # TODO
     email = Email(config.get('door.email.address'), config.get('door.email.pword'), config.get_conf_file_contents('email-style.css'))
     challenge = Challenge(config, db, sms, email)
     dropbox = Dropbox(config.get('dropbox.gd.access.token'))
-    command = GarageDoorCommand(config, db, challenge, pi, sms, email, dropbox)
+    command = GarageDoorCommand(config, db, challenge, pi, doorRemote, doorState, camera, sms, email, dropbox)
 
     logging.info("Starting SMS Monitor Application")
     while True:
         try:
             # Turn on green light to indicate that work is being done
-            pi.green_light_on()
+            light.green_light_on()
 
             # fetch all the new messages that need processing
             smsMessages = sms.list()
@@ -71,16 +79,16 @@ def main():
                 command.execute(message)
 
             # Clear any warn/error indicators
-            pi.yellow_light_off()
-            pi.red_light_off()
+            light.yellow_light_off()
+            light.red_light_off()
         except NetworkDownException as nde:
             logging.error('Unable to reach network, cannot process sms command messages', exc_info=True)
-            pi.yellow_light_on()
+            light.yellow_light_on()
         except:
             logging.error('Failure processing sms command messages', exc_info=True)
-            pi.red_light_on()
+            light.red_light_on()
         finally:
-            pi.green_light_off()
+            light.green_light_off()
             time.sleep(5)
 
 def get_config_directory(args, default):
